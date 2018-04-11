2700424fc1cf81b7166100148f328568fe78a249
Julien Bourdin <jbourdin@gmail.com>
2018-04-12 00:06:47 +0200

    push v1.1 from store

--- toggle.php
+++ toggle.php
@@ -1,22 +1,30 @@
 <?php
    //***********************************************************************************************************************
-   // V1.0 : Script qui permet la bascule d'un périphérique
+   // V1.0 : Script qui permet la bascule d'un pï¿½riphï¿½rique : influman
+   // V1.1 : rajout possible des valeurs ON et Off dans VAR2 et VAR3 : Merguez07
    //*************************************** API eedomus ******************************************************************
-   
+
    // recuperation des infos depuis la requete
-  
-  $periphId=getArg("periph");
 
-
-   
-if ($periphId != '' and $periphId != 'plugin.parameters.device_id') {
-    $periphvalue = getValue($periphId);  
-	if  ($periphvalue['value'] == 0) {
-		setValue($periphId, 100, $verify_value_list = false, $update_only = false);
-   	} else { 
-		setValue($periphId, 0, $verify_value_list = false, $update_only = false);
+    $periphId=getArg("periph");
+    $val1 = getArg("val1");
+    $val2 = getArg("val2");
+    if ($val1 == '') {
+        $val1 = 0;
     }
-} 
+
+    if ($val2 == '') {
+        $val2 = 100;
+    }
+
+    if ($periphId != '' and $periphId != 'plugin.parameters.device_id') {
+        $periphvalue = getValue($periphId);
+      if  ($periphvalue['value'] == $val1) {
+        setValue($periphId, $val2, $verify_value_list = false, $update_only = false);
+        } else {
+        setValue($periphId, $val1, $verify_value_list = false, $update_only = false);
+        }
+    }
 
 die();
 ?>
